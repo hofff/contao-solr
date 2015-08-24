38257745bdf7ebbc65bff5909f8efa0a5b76be8b
Oliver Hoff <oliver@hofff.com>
2015-08-24 02:11:37 +0200

    indexing of multivalued query params for easier query building

--- src/Hofff/Contao/Solr/Index/Query.php
+++ src/Hofff/Contao/Solr/Index/Query.php
@@ -18,24 +18,49 @@ class Query {
 		return $this->params;
 	}
 
-	public function hasParam($name) {
-		return isset($this->params[$name]);
+	public function hasParam($name, $key = null) {
+		if($key === null) {
+			return isset($this->params[$name]);
+		} else {
+			return isset($this->params[$name][$key]);
+		}
 	}
 
-	public function getParam($name) {
-		return $this->hasParam($name) ? $this->params[$name] : null;
+	public function getParam($name, $key = null) {
+		if(!$this->hasParam($name, $key)) {
+			return null;
+		} elseif($key === null) {
+			return $this->params[$name];
+		} else {
+			return $this->params[$name][$key];
+		}
 	}
 
-	public function setParam($name, $value) {
-		$this->params[$name] = array($value);
+	public function setParam($name, $value, $key = null) {
+		if($key === null) {
+			$this->params[$name] = array($value);
+		} else {
+			$this->params[$name] = array($key => $value);
+		}
 	}
 
-	public function addParam($name, $value) {
-		$this->params[$name][] = $value;
+	public function addParam($name, $value, $key = null) {
+		if($key === null) {
+			$this->params[$name][] = $value;
+		} else {
+			$this->params[$name][$key] = $value;
+		}
 	}
 
-	public function removeParam($name) {
-		unset($this->params[$name]);
+	public function removeParam($name, $key = null) {
+		if($key !== null) {
+			unset($this->params[$name]);
+		} else {
+			unset($this->params[$name][$key]);
+			if(!$this->params[$name]) {
+				unset($this->params[$name]);
+			}
+		}
 	}
 
 	public function hasContent() {
